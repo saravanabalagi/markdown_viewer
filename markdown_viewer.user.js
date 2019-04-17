5cef49642d4a8a05a85fbe4755475736f0af8a8c
Saravanabalagi Ramachandran <saravanabalagi@hotmail.com>
2019-04-17 13:20:14 +0100

    Update markdown_viewer.user.js
    Adding styles

--- markdown_viewer.user.js
+++ markdown_viewer.user.js
@@ -16,23 +16,34 @@ let markdownPre = document.getElementsByTagName('pre')[0];
 
 let markdownParentNode = document.createElement("div");
 markdownParentNode.innerHTML = `
-<div id="readme" class="Box Box--condensed instapaper_body md js-code-block-container">
-    <div class="Box-header d-flex flex-items-center flex-justify-between px-2">
-      <h3 class="Box-title pr-3">
-        <svg class="octicon octicon-book" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M3 5h4v1H3V5zm0 3h4V7H3v1zm0 2h4V9H3v1zm11-5h-4v1h4V5zm0 2h-4v1h4V7zm0 2h-4v1h4V9zm2-6v9c0 .55-.45 1-1 1H9.5l-1 1-1-1H2c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h5.5l1 1 1-1H15c.55 0 1 .45 1 1zm-8 .5L7.5 3H2v9h6V3.5zm7-.5H9.5l-.5.5V12h6V3z"></path></svg>
-        readme.md
-      </h3>
-    </div>
-      <div class="Box-body">
-        <article class="markdown-body entry-content p-5" itemprop="text">
-        </article>
+<div class="container" style="padding:24px">
+  <div id="readme" class="Box Box--condensed instapaper_body md js-code-block-container">
+      <div class="Box-header d-flex flex-items-center flex-justify-between px-2">
+        <h3 class="Box-title pr-3">
+          <span class="octicon octicon-clippy"></span>
+          <svg class="octicon octicon-book" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M3 5h4v1H3V5zm0 3h4V7H3v1zm0 2h4V9H3v1zm11-5h-4v1h4V5zm0 2h-4v1h4V7zm0 2h-4v1h4V9zm2-6v9c0 .55-.45 1-1 1H9.5l-1 1-1-1H2c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h5.5l1 1 1-1H15c.55 0 1 .45 1 1zm-8 .5L7.5 3H2v9h6V3.5zm7-.5H9.5l-.5.5V12h6V3z"></path></svg>
+          <span class="Box-title-span"></span>
+        </h3>
       </div>
-    </div>
-<div>
+        <div class="Box-body">
+          <article class="markdown-body entry-content p-5" itemprop="text">
+          </article>
+        </div>
+      </div>
+  <div>
+</div>
 `
 
 let markdownNode = markdownParentNode.getElementsByClassName("markdown-body")[0];
+
+let titleNode = markdownParentNode.getElementsByClassName("Box-title-span")[0];
+let documentTitle = document.location.href;
+let documentTitleTokens = documentTitle.split("/");
+let filename = documentTitleTokens[documentTitleTokens.length - 1];
+titleNode.innerHTML = filename;
+
 let html = converter.makeHtml(markdownPre.innerHTML);
 markdownNode.innerHTML = html;
+
 bodyNode.appendChild(markdownParentNode);
 bodyNode.removeChild(markdownPre);
